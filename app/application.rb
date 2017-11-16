f89242718e9d9aa248050577fc0ea2da90d4c051
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-16 19:15:13 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+
+    if time.hour > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+
+    resp.finish
+  end
+
+end
